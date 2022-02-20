205f53802d6f39ec4aa186a77a48a23780317b68
Ryan <jukkasc2@gmail.com>
2022-02-19 22:14:28 -0500

    added smooth behaviour to html and removed react-scroll

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -8,49 +8,45 @@ import {
   Typography,
 } from "@material-ui/core";
 
-import { Link } from "react-scroll";
-
 const MovieList = ({ movies, plots, setSelectedMovie }) => {
   console.log(plots);
   return (
     <Box sx={{ flexGrow: 1 }}>
       <Grid container spacing={2} justify="center">
         {movies?.map((movie, index) => (
-          <Link to="main" smooth={true} duration={1000}>
-            <Grid
-              item
-              xs="auto"
-              // onClick={() => {
-              //   setSelectedMovie(movie.imdbID);
-              //   window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
-              // }}
+          <Grid
+            item
+            xs="auto"
+            onClick={() => {
+              setSelectedMovie(movie.imdbID);
+              window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+            }}
+          >
+            <Card
+              elevation={6}
+              style={{
+                height: 600,
+                width: 300,
+              }}
+              className="card"
             >
-              <Card
-                elevation={6}
-                style={{
-                  height: 600,
-                  width: 300,
-                }}
-                className="card"
-              >
-                <CardMedia
-                  style={{ height: 450, width: 300 }}
-                  image={movie.Poster}
-                  alt={movie.Title}
-                  key={index}
-                />
-                <CardContent>
-                  <Typography gutterBottom variant="h5">
-                    {movie.Title}
-                  </Typography>
-                  <Typography gutterBottom variant="body1">
-                    {movie.Year}
-                  </Typography>
-                  <Typography gutterBottom variant="body2"></Typography>
-                </CardContent>
-              </Card>
-            </Grid>
-          </Link>
+              <CardMedia
+                style={{ height: 450, width: 300 }}
+                image={movie.Poster}
+                alt={movie.Title}
+                key={index}
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5">
+                  {movie.Title}
+                </Typography>
+                <Typography gutterBottom variant="body1">
+                  {movie.Year}
+                </Typography>
+                <Typography gutterBottom variant="body2"></Typography>
+              </CardContent>
+            </Card>
+          </Grid>
         ))}
       </Grid>
     </Box>
